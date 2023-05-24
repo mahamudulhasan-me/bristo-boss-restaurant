0bef7178b68729aad417e0e92fe5db25e912f6eb
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-05-24 14:47:25 +0600

    add popular menu

--- src/components/Home/MenuItems/MenuItems.jsx
+++ src/components/Home/MenuItems/MenuItems.jsx
@@ -1,19 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import useMenu from "../../../hooks/useMenu";
 import MenuItem from "../../Shared/MenuItem/MenuItem";
 import SectionHeader from "../../Shared/SectionHeader/SectionHeader";
 
 const MenuItems = () => {
-  const [menus, setMenu] = useState([]);
-  useEffect(() => {
-    fetch(`menu.json`)
-      .then((response) => response.json())
-      .then((data) => setMenu(data));
-  }, []);
+  const [menuItems] = useMenu();
+  const popularMenuItems = menuItems.filter(
+    (item) => item.category === "popular"
+  );
+
   return (
     <div className="w-4/5 mx-auto my-20">
       <SectionHeader title="FROM OUR MENU" subTitle="Check It Out" />
       <div className="grid grid-cols-2 gap-10">
-        {menus.slice(0, 6).map((menu) => (
+        {popularMenuItems.map((menu) => (
           <MenuItem key={menu._id} menuDetails={menu} />
         ))}
         <div className="col-span-2 mx-auto">
--- src/components/Pages/OurMenu/OurMenu.jsx
+++ src/components/Pages/OurMenu/OurMenu.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import banner from "../../../assets/menu/banner3.jpg";
+import useMenu from "../../../hooks/useMenu";
 import PageTopBanner from "../../Shared/PageTopBanner/PageTopBanner";
+import OfferMenu from "./OfferMenu/OfferMenu";
 
 const OurMenu = () => {
+  const [allMenu] = useMenu();
+  const offeredMenu = allMenu.filter((menu) => menu.category === "offered");
+  const pizzaMenu = allMenu.filter((menu) => menu.category === "pizza");
+  const dessertMenu = allMenu.filter((menu) => menu.category === "dessert");
+  const saladMenu = allMenu.filter((menu) => menu.category === "salad");
+  const soupMenu = allMenu.filter((menu) => menu.category === "soupMenu");
   return (
     <div>
       <PageTopBanner
@@ -10,6 +18,7 @@ const OurMenu = () => {
         title="Our Menu"
         subtitle="Would you like to try a dish?"
       />
+      <OfferMenu menuItems={offeredMenu} />
     </div>
   );
 };
