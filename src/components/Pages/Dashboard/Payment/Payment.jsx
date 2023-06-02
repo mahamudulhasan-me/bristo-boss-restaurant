8c2b59fb17d999593a8ab5dd079aa6e9788205b7
mahamudulhasan-me <mahamudulhasan.org@gmail.com>
2023-06-03 00:01:08 +0600

    add payment form

--- src/components/Pages/Dashboard/Payment/Payment.jsx
+++ src/components/Pages/Dashboard/Payment/Payment.jsx
@@ -1,10 +1,21 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 import React from "react";
 import SectionHeader from "../../../Shared/SectionHeader/SectionHeader";
+import CheckoutForm from "./CheckoutForm";
 
+const stripePromise = loadStripe(
+  "pk_test_51NEWqJBqXwzigbXRP470KDyMHym9vZ2BX4IWTtW2rKgVi70RgduKN7VyUy5ThSS77Om0vAd212ETrKNoNcl5ndw900IHFiKIg3"
+);
 const Payment = () => {
   return (
     <div>
       <SectionHeader title={"Payment"} subTitle={"O Teka Teka"} />
+      <div>
+        <Elements stripe={stripePromise}>
+          <CheckoutForm />
+        </Elements>
+      </div>
     </div>
   );
 };
